94825b1249d6e873863b179e08bbf709a8a81a0b
pulpstudent <120042356+pulpstudent@users.noreply.github.com>
2023-11-10 11:36:17 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 # id_test = -1001658066382
 import requests
 
-update_url = 'https://api.telegram.org/bot6453932148:AAFeB8vjo2ly_UCWlAaDHgS3VhQpvfDdAQ4/sendPhoto'
+update_url = 'https://api.telegram.org/bot6453932148:AAFeB8vjo2ly_UCWlAaDHgS3VhQpvfDdAQ4/sendMessage'
 parameters = {
       "chat_id" : "-1001658066382" ,
-      "photo" : "" ,
-      "caption" : "\n" + "\n" + "Changes : " + "\n" + "1.RUN2.2" + "\n" + "#ggdevs"
+      "text" : "\n" + "\n" + "Changes : " + "\n" + "111.RUN2.2" + "\n" + "#ggdevs" ,
+      "parse_mode" : "markdown"
 }
 
 resp_update = requests.get(update_url, data= parameters)
